ce2a56779b4a5e12beee3df1a4eba73c5780154d
darko <darkonikoloc@gmail.com>
2023-10-24 21:09:13 +0200

    origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,10 @@
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://dqrk0jeste.github.io/blog'
+  'https://dqrk0jeste.github.io',
+  'https://dqrk0jeste.github.io/',
+  'https://dqrk0jeste.github.io/blog',
+  'https://dqrk0jeste.github.io/blog/'
+
 ];
 
 module.exports = allowedOrigins
