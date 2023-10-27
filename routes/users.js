6104ed166685e4a58713053155b7aa6b30cd59a1
darko <darkonikoloc@gmail.com>
2023-10-27 19:43:24 +0200

    fix for current

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const handleLogout = require('../controllers/logoutController')
 const { verifyUser } = require('../middleware/verifyUser')
 const User = require('../database/User')
 
-router.get('/current', verifyUser, (req, res) => res.json({ user: req.user }))
+router.post('/current', verifyUser, (req, res) => res.json({ user: req.user }))
 
 router.post('/register', handleRegistation)
 router.post('/login', handleLogin)
